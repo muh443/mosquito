77830f0786e39730d02a63733e3880234f21d58b
muh443 <128766981+muh443@users.noreply.github.com>
2023-03-24 18:12:35 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash 
+ sudo apt-get install openjdk-17-jdk -y 
+ sudo apt-get install aapt zipalign -y 
+ sudo apt-get install apktool -y 
+ sudo apt-get install imagemagick -y 
+ sudo apt-get install python3 python3-pip -y 
+ pip install Pillow
+ figlet done
